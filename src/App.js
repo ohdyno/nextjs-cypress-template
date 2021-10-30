e7954f982eeef649ba7d7ef9ebf4208fb40caab5
Xing Zhou <xing@xingzhou.me>
2021-10-29 21:54:53 -0400

    create examples demonstrating testing for a focused UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultInitialState = {text: 'data'};
+const defaultReducer = (state, action) => ({text: action.payload})
+
+
+function App({initialState = defaultInitialState, reducer = defaultReducer}) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function selectAll(e) {
+        e.target.select();
+    }
+
+    return (
+        <div>
+            <label>input
+                <input value={state.text} onFocus={selectAll} onChange={(event) => {
+                    dispatch({
+                        type: 'correct action name',
+                        payload: event.target.value
+                    })
+                    event.preventDefault();
+                }}/>
+            </label>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function spyOnReducerAction(reducer) {
+    const captured = {result: undefined};
+    return {
+        captured,
+        reducer: (state, action) => {
+            captured.result = action;
+            return reducer(state, action);
+        }
+    };
+}
+
+describe('Simplify UI into two responsibilities using reducers', () => {
+    it('renders data from props', () => {
+        const initialState = {text: "some data"};
+
+        render(<App initialState={initialState}/>);
+
+        const renderedData = screen.getByDisplayValue(new RegExp(`${initialState.text}`, 'i'));
+        expect(renderedData).toBeInTheDocument();
+    });
+
+    it('dispatches the correct action when button is clicked', () => {
+        const {captured, reducer} = spyOnReducerAction((state, action) => ({text: action.payload}));
+        render(<App reducer={reducer} initialState={{text: 'data'}}/>);
+
+        const input = screen.getByLabelText(/input/i);
+        userEvent.type(input, 'some new data');
+
+        expect(captured.result).toEqual({type: 'correct action name', payload: 'some new data'});
+    });
+})
